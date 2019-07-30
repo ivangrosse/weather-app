cab2558f2c23b0ba51b12a45d55ad238ed5780dd
Ivan <ivangrosso@gmail.com>
2019-07-30 15:40:57 -0300

    Changes to forecast to include max and min temperatures for the day

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find that location.', undefined);
         }else{
-            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability + '% chance of rain.');
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability + '% chance of rain. Max temperature: ' + body.daily.data[0].temperatureHigh + '. Min temperature: ' + body.daily.data[0].temperatureLow);
         }
     });
 };
